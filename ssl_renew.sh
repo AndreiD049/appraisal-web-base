ab4bd729b96235944a86752c54b14196905887b8
AndreiD049 <andrei.dimitrascu94@gmail.com>
2020-11-15 02:26:47 +0200

    remove intermediate container

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/local/bin/docker-compose -f /appraisal-web-base/docker-compose.yml run certbot renew \
+/usr/local/bin/docker-compose -f /appraisal-web-base/docker-compose.yml run --rm certbot renew \
 && /usr/local/bin/docker-compose -f /appraisal-web-base/docker-compose.yml kill -s SIGHUP web_front
